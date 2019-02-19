a11198bc75e32fcd56388f5a9263263aa9980373
ACARD MARY <maryacard@hotmail.com>
2019-02-19 12:48:53 +0100

    stashing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,38 @@
 class BookingsController < ApplicationController
   def index
+    @bookings = Booking.all
   end
 
   def show
+    # @booking = Booking.find(params[:id])
   end
 
   def new
+    # @booking = Booking.new()
+    # @service = Service.find(params[:service_id])
   end
 
   def create
+    # @booking = Booking.new(booking_params)
+    # @service = Service.find(params[:service_id])
+    # @booking.service = @service
+    # if @booking.save
+    #   redirect_to booking_path(@bookings)
+    # else
+    #   render :new
+    # end
   end
 
   def destroy
+        # @booking = Booking.find(params[:id])
+        # @booking.destroy
+        # redirect_to booking_path(@bookings)
   end
+
+  private
+
+  def booking_params
+    #params.require(:booking).permit(:quantity, :place, :user_id, :service_id)
+  end
+
 end
