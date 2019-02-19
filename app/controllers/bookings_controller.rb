bb1ceadb129dd7c1f5781d64220e65fecc603b46
ACARD MARY <maryacard@hotmail.com>
2019-02-19 15:52:33 +0100

    booking controller updated

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,35 +4,35 @@ class BookingsController < ApplicationController
   end
 
   def show
-    # @booking = Booking.find(params[:id])
+    @booking = Booking.find(params[:id])
   end
 
-  def new
-    # @booking = Booking.new()
-    # @service = Service.find(params[:service_id])
-  end
+  # def new
+  #   @booking = Booking.new()
+  #   @service = Service.find(params[:service_id])
+  # end
 
   def create
-    # @booking = Booking.new(booking_params)
-    # @service = Service.find(params[:service_id])
-    # @booking.service = @service
-    # if @booking.save
-    #   redirect_to booking_path(@bookings)
-    # else
-    #   render :new
-    # end
+    @booking = Booking.new(booking_params)
+    @service = Service.find(params[:service_id])
+    @booking.service = @service
+    if @booking.save
+      redirect_to booking_path(@bookings)
+    else
+      render :new
+    end
   end
 
   def destroy
-        # @booking = Booking.find(params[:id])
-        # @booking.destroy
-        # redirect_to booking_path(@bookings)
+        @booking = Booking.find(params[:id])
+        @booking.destroy
+        redirect_to booking_path(@bookings)
   end
 
   private
 
   def booking_params
-    #params.require(:booking).permit(:quantity, :place, :user_id, :service_id)
+    params.require(:booking).permit(:quantity, :place, :user_id, :service_id)
   end
 
 end
