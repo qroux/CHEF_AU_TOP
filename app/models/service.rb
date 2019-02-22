b5b0b0d1ac25c3982cab026e920f3d7c18170e0d
Milena Gaigne <milena.gaigne@gmail.com>
2019-02-22 11:39:55 +0100

    ajustements

--- app/models/service.rb
+++ app/models/service.rb
@@ -2,6 +2,7 @@ class Service < ApplicationRecord
   belongs_to :user
   has_many :bookings
   mount_uploader :picture, PhotoUploader
+  validates :picture, presence: true
   include PgSearch
     pg_search_scope :search_by_menu_name_and_category,
     against: [:menu_name, :category],
