8d05f11d8696377ef9b001bf7df002b3d838c7e5
Milena Gaigne <milena.gaigne@gmail.com>
2019-02-22 12:12:31 +0100

    we can book only one time a service and the quantity is managed

--- app/models/service.rb
+++ app/models/service.rb
@@ -4,9 +4,9 @@ class Service < ApplicationRecord
   mount_uploader :picture, PhotoUploader
   validates :picture, presence: true
   include PgSearch
-    pg_search_scope :search_by_menu_name_and_category,
+  pg_search_scope :search_by_menu_name_and_category,
     against: [:menu_name, :category],
     using: {
       tsearch: { prefix: true } # <-- now `superman batm` will return something!
-    }
+  }
 end
